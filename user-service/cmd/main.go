d433f63dfb25e6db3572d149745a73a36c682815
Hasindu Muhandiram <hasindumuhandiram45@gmail.com>
2026-03-26 13:37:41 +0530

    chore: upgrade go toolchain to 1.25 and golangci-lint to v1.64.8

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"log/slog"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func main() {
@@ -17,6 +23,46 @@ func main() {
 		"service", "user-service",
 	)
 
+	dbURL := os.Getenv("DATABASE_URL")
+
+	if dbURL == "" {
+		slog.Error("DATABASE_URL is not set")
+		os.Exit(1)
+	}
+
+	// connect to PostgreSQL
+	database, err := sql.Open("postgres", dbURL)
+
+	if err != nil {
+		slog.Error("error connecting to the database", "error", err)
+		os.Exit(1)
+	}
+
+	// verify connection
+	if err := database.Ping(); err != nil {
+		slog.Error("failed to ping database",
+			"error", err,
+		)
+		os.Exit(1)
+	}
+
+	slog.Info("database connected successfully")
+
+	// Run migrations
+	m, err := migrate.New("file://db/migrations", dbURL)
+	if err != nil {
+		slog.Error("failed to initialize migrations", "error", err)
+		os.Exit(1)
+	}
+
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		slog.Error("failed to initialize migrations", "error", err)
+	}
+
+	slog.Info("migrations applied successfully",
+		"service", "user-service",
+	)
+
 	// Wait for shutdown signal
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
@@ -26,4 +72,11 @@ func main() {
 	slog.Info("service shutting down",
 		"service", "user-service",
 	)
+
+	// close DB connection gracefully
+	if err := database.Close(); err != nil {
+		slog.Error("failed to close database connection",
+			"error", err,
+		)
+	}
 }
